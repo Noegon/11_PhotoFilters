88c671019a1d96ed32e31d628b5c461a2aaea16d
Alexey Stafeyev <noegon@yandex.ru>
2017-08-28 23:14:32 +0300

    Initial Commit

---
+++ 11_PhotoFilters/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  11_PhotoFilters
+//
+//  Created by Alexey Stafeyev on 28.08.17.
+//  Copyright © 2017 Alexey Stafeyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 11_PhotoFilters/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  11_PhotoFilters
+//
+//  Created by Alexey Stafeyev on 28.08.17.
+//  Copyright © 2017 Alexey Stafeyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
